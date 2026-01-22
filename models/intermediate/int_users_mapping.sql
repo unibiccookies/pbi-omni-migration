4b8339d34d46203c108f76659e19da9b097cd18a
Asmita Dwivedi <asmitadwivedi024@gmail.com>
2026-01-22 06:52:06 +0000

    added dimensions

--- models/intermediate/int_users_mapping.sql
+++ models/intermediate/int_users_mapping.sql
@@ -7,18 +7,12 @@ users as (
 )
 
 select
-    -- 1. The Link to Costs
     mapping.target_bus as payer_group,
     
-    -- 2. The Context
     mapping.agreement_name,
     mapping.agreement_key,
 
-    -- 3. The Enriched User Info
     users.full_name as owner_name,
-    
-    -- FIX: Use 'username' instead of 'email', but keep the alias 'owner_email'
-    -- so we don't break the next table.
     users.username as owner_email, 
     
     users.department_name,
